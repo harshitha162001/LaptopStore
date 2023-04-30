a5633e60a955e1441b5a4f1f08833101d886f9a3
harshitha <harshithaaithal16@gmail.com>
2023-04-30 08:13:23 +0530

    final commit

---
+++ activity1/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace activity1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Confirmation>Confirmations{get;set;}
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=BSC-PG02KEHR\\SQLEXPRESS;DataBase=ActivityDB;Integrated Security=true;TrustServerCertificate=true");
+        }
+    }
+}
---
+++ activity1/Models/Brand.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace activity1.Models
+{
+    public class Brand
+    {
+        public int BrandId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime BrandAddedDate { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
